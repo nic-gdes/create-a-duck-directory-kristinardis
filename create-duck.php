2597e16fc065a23f74ab82a4fb3a5a71788461d0
kristinardis <kamcllvenna@nic.edu>
2024-02-17 14:17:58 -0800

    Created. access to images on database, see below.
    Simplified validation into if/else if statements, and retrieved binary data of image from temp file to insert in database.  Once again, my. son sorted out the problem with the images.

--- create-duck.php
+++ create-duck.php
@@ -1,110 +1,89 @@
-<?php
-//check for POST
-if (isset($_POST['submit'])) {
-
-    // create error array
-    $errors = ["name" => '', "favorite_foods" => '', "bio" => ''];
-
-
-    $name = htmlspecialchars($_POST["name"]);
-    $favorite_foods = htmlspecialchars($_POST["favorite_foods"]);
-    $bio = htmlspecialchars($_POST"bio"]);
-
-
-    if (empty($name)) {
-        $errors["name"] = 'A name is required';
-    } else {
-        if (preg_match('/^[a-z,\s]+$/i', $name)) {
-            $errors["name"] = 'The name has illegal characters';
-        }
-    }
-        if (empty($favorite_foods)) {
-            $errors["favorite_foods"] = "You are hungry";
-        } else {
-            if (!preg_match('/^[a-z,\s]+$/i', $favorite_foods)) {
-                $errors["favorite_foods"] = 'Favorite foods must be a comma separated list';
-            }
-        }
-            if (empty($bio)) {
-                $errors["bio"] = 'You must have a bio';
-            }
-
-            //if (preg_match('/^[a-z,\s]+$/i', $bio)) {
-               // $errors['bio'] = 'Bio must be a string';
-           // }
-            if (!array_filter($errors)) {
-           //  form is valid
-                echo "submitted successfully";    
-                ?>
-
-                //connect to db
-                <?php
-
-                require("./config/db.php");
-                //build sql query
-
-                $sql = "INSERT INTO ducks(name, favorite_foods, bio) VALUES ("$name", "$favorite_foods", "$bio")";
-               
-
-                //execute query in mysql
-                mysqli_query($conn, $sql);
-
-               
-
-                //load home page
-                
-                header("Location:index.php");
-
-            } else {
-                //if there are any errors
-                echo "errors in the form: ";
-            }   
-
-        }
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
+
+<?php require("./config/db.php"); ?>
 <?php include 'components/head.php'; ?>
 
 <body>
     <?php include 'components/nav.php'; ?>
     <?php
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $showForm = true;
+    $name = "";
+    $favorite_foods = "";
+    $bio = "";
 
-        $duckName = $_POST["duck_name"];
-        $favoriteFoods = $_POST["favorite_foods"];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {// create error array
+        $errors = ["name" => '', "favorite_foods" => '', "bio" => ''];
+        $name = htmlspecialchars($_POST["name"]);
+        $favorite_foods = htmlspecialchars($_POST["favorite_foods"]);
+        $bio = htmlspecialchars($_POST["biography"]);
+        $nameMatch = preg_match('/^[a-z0-9\s]+$/i', $name);
+        $favoriteFoodsMatch = preg_match('/^[a-z0-9\s]+$/i', $favorite_foods);
+        $bioMatch = preg_match('/^[a-z0-9\s]+$/i', $bio);
+    
+        if (empty($name)) {
+            $errors["name"] = 'A name is required';
+        } else if (!$nameMatch) {
+            $errors["name"] = 'The name has illegal characters';
+        }
+    
+        if (empty($favorite_foods)) {
+            $errors["favorite_foods"] = "You are hungry";
+        } else if (!$favoriteFoodsMatch) {
+            $errors["favorite_foods"] = 'Favorite foods has illegal characters';
+        }
+    
+        if (empty($bio)) {
+            $errors["bio"] = 'You must have a bio';
+        } else if (!$bioMatch) {
+            $errors['bio'] = 'Bio has illegal characters';
+        }
+    
+        $success = empty($errors['name']) && empty($errors['favorite_foods']) && empty($errors['bio']);
+    
+        if ($success) {
+            $imageBinary = file_get_contents($_FILES['duck_image']['tmp_name']);
+            $image = mysqli_real_escape_string($conn, $imageBinary);
+            $name = mysqli_real_escape_string($conn, $name);
+            $favorite_foods = mysqli_real_escape_string($conn, $favorite_foods);
+            $bio = mysqli_real_escape_string($conn, $bio);
 
-        $duckImage = $_FILES["duck_image"]["name"];
-        $biography = $_POST["biography"];
+            $sql = "INSERT INTO ducks (name, favorite_foods, biography, image, created_at) 
+                    VALUES ('$name', '$favorite_foods', '$bio', '$image', NOW())";
 
-
-        echo "<h2>Submitted Duck Information</h2>";
-        echo "<p><strong>Duck Name:</strong> $duckName</p>";
-        echo "<p><strong>Favorite Foods:</strong> $favoriteFoods</p>";
-        echo "<p><strong>Duck Image:</strong> $duckImage</p>";
-        echo "<p><strong>Biography:</strong> $biography</p>";
-    } else {
-
+            if ($conn->query($sql) === TRUE) {
+                echo "Duck created successfully!";
+                $showForm = false;
+                header("Location:index.php");
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        } else {
+            echo "There are errors in the form:";
+            
+            foreach ($errors as $error) {
+                echo "<br/>" . $error;
+            }
+        }
+    }
+    
+    if ($showForm) {
         ?>
 
         <form action="create-duck.php" method="POST" enctype="multipart/form-data">
 
-            <label for="duck_name">Duck Name:</label>
-            <input type="text" id="duck_name" name="duck_name" required>
+            <label for="name">Duck Name:</label>
+            <input type="text" id="name" name="name" value="<?php echo $name ?>" required>
 
             <label for="favorite_foods">Favorite Foods:</label>
-            <textarea id="favorite_foods" name="favorite_foods" rows="5" required></textarea>
+            <textarea id="favorite_foods" name="favorite_foods" rows="5" required><?php echo $favorite_foods ?></textarea>
 
             <label for="duck_image">Duck Image:</label>
             <input type="file" id="duck_image" name="duck_image" accept="image/*" required>
 
             <label for="biography">Biography:</label>
-            <textarea id="biography" name="biography" rows="15" required></textarea>
+            <textarea id="biography" name="biography" rows="15" required><?php echo $bio ?></textarea>
 
             <button type="submit">Submit</button>
         </form>
