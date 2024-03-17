d97531b0bdd12632bed13b744b55dd1920d2de60
kristinardis <kamcllvenna@nic.edu>
2024-03-16 17:22:25 -0700

    submission 9.1

--- profile.php
+++ profile.php
@@ -43,7 +43,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profile of my Duck</title>
 </head>
 
 <body>
